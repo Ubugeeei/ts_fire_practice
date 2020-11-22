e3be91d93009b77b28c467b6c677762786c6eb4b
Koji Nishimura <71201308+Ubugeeei@users.noreply.github.com>
2020-11-22 23:12:07 +0900

    APIキーなどコピペ

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -2,7 +2,16 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 if (!firebase.apps.length) {
-  firebase.initializeApp({});
+  firebase.initializeApp({
+    apiKey: "ここの値は人それぞれ違います",
+    authDomain: "ここの値は人それぞれ違います",
+    databaseURL: "ここの値は人それぞれ違います",
+    projectId: "ここの値は人それぞれ違います",
+    storageBucket: "ここの値は人それぞれ違います",
+    messagingSenderId: "ここの値は人それぞれ違います",
+    appId: "ここの値は人それぞれ違います",
+    measurementId: "ここの値は人それぞれ違います"
+  });
   firebase.auth().useDeviceLanguage();
 }
 export default firebase;
